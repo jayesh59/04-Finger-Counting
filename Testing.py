1b93aa1ce12e61e00029dbc2a3cc6e3266e0afa9
jayesh59 <51175318+jayesh59@users.noreply.github.com>
2020-04-15 19:51:50 +0530

    Update Testing.py
    still the error persist
    
    so tried checking the shape of array again
    but it was still 3d array
    
    then i thought that i need to convert tthe array to np.int32 array
    
    but still not.
    idk what the error is right now

--- Testing.py
+++ Testing.py
@@ -10,18 +10,18 @@ obj = cv2.CascadeClassifier('data/haarcascade/palm.xml')
 
 while True:
     ret,frame = cam.read()
-    
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    frame = np.int32(frame)
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-   # hand_rect = obj.detectMultiScale(gray , scaleFactor = 1.2 , minNeighbors = 5)
+    hand_rect = obj.detectMultiScale(frame , scaleFactor = 1.2 , minNeighbors = 5)
 
-    cv2.imshow('frame', gray)
+    cv2.imshow('frame', frame)
 
     #for (x,y,w,h) in hand_rect:
-        #cv2.rectangle(frame, (x,y) , (x+w,y+h), 255, 5)
+     #   cv2.rectangle(frame, (x,y) , (x+w,y+h), 255, 5)
 
     if cv2.waitKey(10) & 0xFF == 27:
-        print(gray.shape)
+        print(frame.shape)
         break
 
 cam.release()
