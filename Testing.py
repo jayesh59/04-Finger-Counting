4dc6acb66b63203c74b9895a896add671faa6906
jayesh59 <51175318+jayesh59@users.noreply.github.com>
2020-04-15 21:50:37 +0530

    Again new Changes
    so the next issue was that no rectangle was being made around the face, so it was again a stupid mistake because i was not printing the frame after the for loop but after re assigning the frame afrer reading from the web cam. so it was continuously displaying a new frame rather than the frame it was drawing upon

--- Testing.py
+++ Testing.py
@@ -5,21 +5,19 @@ import matplotlib.pyplot as plt
 cam = cv2.VideoCapture(0)
 w = cam.get(cv2.CAP_PROP_FRAME_WIDTH)
 h = cam.get(cv2.CAP_PROP_FRAME_HEIGHT)
-obj = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+obj = cv2.CascadeClassifier('haarcascade_profileface.xml')
 
 
 while True:
     ret,frame = cam.read()
-    #
-    # frame = np.int16(frame)
-    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
 
     hand_rect = obj.detectMultiScale(frame , scaleFactor = 1.2 , minNeighbors = 5)
 
     cv2.imshow('frame', frame)
 
     for (x,y,w,h) in hand_rect:
-       cv2.rectangle(frame, (x,y) , (x+w,y+h), 255, 5)
+       cv2.rectangle(frame, (x,y) , (x+w,y+h), (255,255,255), 5)
 
     if cv2.waitKey(10) & 0xFF == 27:
         print(frame.shape)
