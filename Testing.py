07871627655da694e5066240f4150a2ec5aae3ea
jayesh59 <51175318+jayesh59@users.noreply.github.com>
2020-04-15 21:52:36 +0530

    Update Testing.py
    this works now well as much it should while detectting the face.

--- Testing.py
+++ Testing.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 cam = cv2.VideoCapture(0)
-w = cam.get(cv2.CAP_PROP_FRAME_WIDTH)
+w = cam.get(cv2.CAP_PROP_FRAME_WIDTH)   
 h = cam.get(cv2.CAP_PROP_FRAME_HEIGHT)
 obj = cv2.CascadeClassifier('haarcascade_profileface.xml')
 
@@ -14,11 +14,13 @@ while True:
 
     hand_rect = obj.detectMultiScale(frame , scaleFactor = 1.2 , minNeighbors = 5)
 
-    cv2.imshow('frame', frame)
+    #
 
     for (x,y,w,h) in hand_rect:
-       cv2.rectangle(frame, (x,y) , (x+w,y+h), (255,255,255), 5)
-
+        cv2.rectangle(frame, (x,y) , (x+w,y+h), (255,255,255), 5)
+    
+        cv2.imshow('frame',frame)
+    
     if cv2.waitKey(10) & 0xFF == 27:
         print(frame.shape)
         break
