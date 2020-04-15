0a0dcad5e5040480e36c479198c45cfdd6de2618
jayesh59 <51175318+jayesh59@users.noreply.github.com>
2020-04-15 21:59:52 +0530

    new files added and testing
    there is this error again - returned a result with an  error set.

--- Testing.py
+++ Testing.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 cam = cv2.VideoCapture(0)
 w = cam.get(cv2.CAP_PROP_FRAME_WIDTH)   
 h = cam.get(cv2.CAP_PROP_FRAME_HEIGHT)
-obj = cv2.CascadeClassifier('haarcascade_profileface.xml')
+obj = cv2.CascadeClassifier('data/haarcascade/palm.xml')
 
 
 while True:
@@ -19,7 +19,7 @@ while True:
     for (x,y,w,h) in hand_rect:
         cv2.rectangle(frame, (x,y) , (x+w,y+h), (255,255,255), 5)
     
-        cv2.imshow('frame',frame)
+    cv2.imshow('frame',frame)
     
     if cv2.waitKey(10) & 0xFF == 27:
         print(frame.shape)
