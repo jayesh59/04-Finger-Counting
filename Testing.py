20b1d0fcba0bf7db144b08bff8a08a5aafe9c049
jayesh59 <51175318+jayesh59@users.noreply.github.com>
2020-04-15 17:37:15 +0530

    Update Testing.py
    this i sadding hand detection to the video but there is this error - returned a result with an error set
    so will continue to debug this and will sort this out now on PC

--- Testing.py
+++ Testing.py
@@ -5,11 +5,18 @@ import matplotlib.pyplot as plt
 cam = cv2.VideoCapture(0)
 w = cam.get(cv2.CAP_PROP_FRAME_WIDTH)
 h = cam.get(cv2.CAP_PROP_FRAME_HEIGHT)
+obj = cv2.CascadeClassifier('data/haarcascade/Hand.Cascade.1.xml')
+
 
 while True:
     ret,frame = cam.read()
     
     cv2.imshow('frame', frame)
+    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    hand_rect = obj.detectMultiScale(frame_gray, 1.2,5)
+
+    for (x,y,w,h) in hand_rect:
+        cv2.rectangle(frame, (x,y) , (x+w,y+h), 255, 5)
 
     if cv2.waitKey(10) & 0xFF == 27:
         break
