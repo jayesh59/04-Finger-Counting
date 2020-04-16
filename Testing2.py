5059bab1053321360e7c910301b0aae127c9741d
jayesh59 <51175318+jayesh59@users.noreply.github.com>
2020-04-16 11:06:04 +0530

    new test commits for new approach
    this is doing the whole process that is done in test1 but the difference is that it is done twice
    1) for taking initial frame that will account for the frame that will be known as the background of the image.
    2) inside the loop
    
    these 2 instances will be then subtracted and will be able to give only the hand
    
    the problem was that there were very fine details that were still being persistant so before final difference added erosion to both the instances.

--- Testing2.py
+++ Testing2.py
@@ -6,10 +6,23 @@ cam = cv2.VideoCapture(0)
 w = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
 h = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
+ret,frame = cam.read()
+blured = cv2.blur(frame.copy(), (3,3))
+gray = cv2.cvtColor(blured, cv2.COLOR_BGR2GRAY)
+roi = gray[h-250:h,w-250:w]
+    
+_ , thresholded = cv2.threshold(roi, 127, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+transformed = cv2.distanceTransform(thresholded, cv2.DIST_L2, 3)
+_ , trans_thresholded40 = cv2.threshold(transformed, 0.40*transformed.max(),255,0)
+_ , trans_thresholded10 = cv2.threshold(transformed, 0.10*transformed.max(),255,0)
+diff1 = cv2.absdiff(trans_thresholded10,trans_thresholded40)
+diff1 = cv2.erode(diff1, (5,5), 2)
+
 while True:
     
     ret,frame = cam.read()
-    blured = cv2.blur(frame, (3,3))
+    blured = cv2.blur(frame.copy(), (3,3))
     gray = cv2.cvtColor(blured, cv2.COLOR_BGR2GRAY)
     roi = gray[h-250:h,w-250:w]
     cv2.rectangle(frame, (w-250,h-250) , (w,h), 255, 5)
@@ -19,23 +32,21 @@ while True:
     transformed = cv2.distanceTransform(thresholded, cv2.DIST_L2, 3)
     _ , trans_thresholded40 = cv2.threshold(transformed, 0.40*transformed.max(),255,0)
     _ , trans_thresholded10 = cv2.threshold(transformed, 0.10*transformed.max(),255,0)
-    diff = cv2.absdiff(trans_thresholded10,trans_thresholded40)
+    diff2= cv2.absdiff(trans_thresholded10,trans_thresholded40)
+    diff2 = cv2.erode(diff2, (5,5), 2)
 
-    
-    thresholded = np.expand_dims(thresholded, axis = 2)
-    transformed = np.expand_dims(transformed, axis = 2)
-    trans_thresholded10 = np.expand_dims(trans_thresholded10, axis = 2)
-    trans_thresholded40 = np.expand_dims(trans_thresholded40, axis = 2)
+
+    diff= cv2.absdiff(diff1,diff2)
+
     diff = np.expand_dims(diff, axis = 2)
 
+    frame[h-250:h,w-250:w] = diff
 
-    frame[h-250:h,w-250:w] = thresholded
     
     cv2.imshow('frame',frame)
     
     if cv2.waitKey(10) & 0xFF == 27:
         print(frame.shape)
-        type(w)
         break
 
 cam.release()
