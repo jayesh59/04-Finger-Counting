1b02a008d39af8fd4da7302cec870e34c716aab0
jayesh59 <51175318+jayesh59@users.noreply.github.com>
2020-04-16 10:25:21 +0530

    Update Testing.py
    trying to isolate fingers as contours from the rest of the hand by subtracting one distance transforming from other.

--- Testing.py
+++ Testing.py
@@ -16,15 +16,19 @@ while True:
     _ , thresholded = cv2.threshold(roi, 127, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
     transformed = cv2.distanceTransform(thresholded, cv2.DIST_L2, 3)
-    _ , trans_thresholded = cv2.threshold(transformed, 0.40*transformed.max(),255,0)
+    _ , trans_thresholded40 = cv2.threshold(transformed, 0.40*transformed.max(),255,0)
+    _ , trans_thresholded10 = cv2.threshold(transformed, 0.10*transformed.max(),255,0)
+    diff = cv2.absdiff(trans_thresholded10,trans_thresholded40)
 
     cv2.rectangle(frame, (w-250,h-250) , (w,h), 255, 5)
     thresholded = np.expand_dims(thresholded, axis = 2)
     transformed = np.expand_dims(transformed, axis = 2)
-    trans_thresholded = np.expand_dims(trans_thresholded, axis = 2)
+    trans_thresholded10 = np.expand_dims(trans_thresholded10, axis = 2)
+    trans_thresholded40 = np.expand_dims(trans_thresholded40, axis = 2)
+    diff = np.expand_dims(diff, axis = 2)
 
 
-    frame[h-250:h,w-250:w] = trans_thresholded
+    frame[h-250:h,w-250:w] = thresholded
     
     cv2.imshow('frame',frame)
     
