f281c10875f085c2a4a36c4f0e9d24ea11187e4c
jayesh59 <51175318+jayesh59@users.noreply.github.com>
2020-04-16 07:53:28 +0530

    Update Testing.py
    tried that method, resulted in only a black screen
    here i was selecting background inside the loop so maybe this was taking the background for like every frame and was not developing any difference so will try and assign the background outside the loop.

--- Testing.py
+++ Testing.py
@@ -5,21 +5,26 @@ import matplotlib.pyplot as plt
 cam = cv2.VideoCapture(0)
 w = cam.get(cv2.CAP_PROP_FRAME_WIDTH)   
 h = cam.get(cv2.CAP_PROP_FRAME_HEIGHT)
-obj = cv2.CascadeClassifier('haarcascade_palm.xml')
+
 
 
 while True:
     ret,frame = cam.read()
+    background = frame.copy().astype('float')
+    diff = cv2.absdiff(background.astype('uint8'), frame)
+
+    _ , thresholded = cv2.threshold(diff, 25, 255, cv2.THRESH_BINARY)
+
+
+
+
     
-
-    hand_rect = obj.detectMultiScale(frame , scaleFactor = 1.2 , minNeighbors = 5)
-
-    #
-
+    '''
     for (x,y,w,h) in hand_rect:
         cv2.rectangle(frame, (x,y) , (x+w,y+h), (255,255,255), 5)
-    
-    cv2.imshow('frame',frame)
+    '''
+
+    cv2.imshow('frame',thresholded)
     
     if cv2.waitKey(10) & 0xFF == 27:
         print(frame.shape)
