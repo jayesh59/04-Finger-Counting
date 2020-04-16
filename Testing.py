0390a3d30d95ee39343e273ba8d309bf1a708302
jayesh59 <51175318+jayesh59@users.noreply.github.com>
2020-04-16 08:50:31 +0530

    Update Testing.py
    so this new approach does not include the dataset, this uses localized thresholding.
    the concept is that therer is a specified area in which the segment of the frame will be thresholded to the hand will be detected and thresholded  here
    
    then the baisc original idea will go on where the contout detection is done for the hand and the difference of frame is used to find the no. of contours.
    
    furter testing is being done, then this will be evolved using the hand detecting dataset so that we can detet hand in any place inside a frame.

--- Testing.py
+++ Testing.py
@@ -3,30 +3,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 cam = cv2.VideoCapture(0)
-w = cam.get(cv2.CAP_PROP_FRAME_WIDTH)   
-h = cam.get(cv2.CAP_PROP_FRAME_HEIGHT)
+w = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
+h = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 while True:
     
     ret,frame = cam.read()
-    #background = frame.copy().astype('float')
     
-    diff = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    _ , thresholded = cv2.threshold(diff, 127, 255, cv2.THRESH_BINARY)
+    gray = cv2.cvtColor(frame.copy(), cv2.COLOR_BGR2GRAY)
+    roi = gray[h-300:h,w-300:w]
+    _ , thresholded = cv2.threshold(roi, 240, 255, cv2.THRESH_BINARY )
 
+    cv2.rectangle(frame, (w-300,h-300) , (w,h), 255, 5)
+    thresholded = np.expand_dims(thresholded, axis = 2)
 
-
-
+    frame[h-300:h,w-300:w] = thresholded
     
-    '''
-    for (x,y,w,h) in hand_rect:
-        cv2.rectangle(frame, (x,y) , (x+w,y+h), (255,255,255), 5)
-    '''
-    
-    cv2.imshow('frame',thresholded)
+    cv2.imshow('frame',frame)
     
     if cv2.waitKey(10) & 0xFF == 27:
         print(frame.shape)
+        type(w)
         break
 
 cam.release()
