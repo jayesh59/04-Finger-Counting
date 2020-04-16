2363e2ac0973b61127b72ecf8fa8466866e800b1
jayesh59 <51175318+jayesh59@users.noreply.github.com>
2020-04-16 08:24:53 +0530

    Update Testing.py
    well not working with this idea anymore, will only work with the dataset only.
    dataset for hands only, but after this project, i will definetely watht he videos on how did he do that project.
    
    here just playing around with
     thresholding

--- Testing.py
+++ Testing.py
@@ -6,17 +6,13 @@ cam = cv2.VideoCapture(0)
 w = cam.get(cv2.CAP_PROP_FRAME_WIDTH)   
 h = cam.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
-ret,frame = cam.read()
-background = frame.copy().astype('float')
-
-
 while True:
     
     ret,frame = cam.read()
     #background = frame.copy().astype('float')
-    diff = cv2.absdiff(background.astype('uint8'), frame)
-    diff = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
-    _ , thresholded = cv2.threshold(diff, 27, 255, cv2.THRESH_BINARY)
+    
+    diff = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    _ , thresholded = cv2.threshold(diff, 127, 255, cv2.THRESH_BINARY)
 
 
 
