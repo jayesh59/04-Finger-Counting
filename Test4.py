32aa44c431091adec7c747b77af19c389539eac9
jayesh59 <51175318+jayesh59@users.noreply.github.com>
2020-04-16 12:24:39 +0530

    Create Test4.py
    test 4 is the file with the previous approach of thresholding first at 2 instances and test 3 i sthe one where thresholding is done at the 1 one instance only. and other processes at one instance only too.

---
+++ Test4.py
@@ -0,0 +1,64 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+cam = cv2.VideoCapture(0)
+w = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
+h = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+ret,frame = cam.read()
+blured = cv2.blur(frame.copy(), (15,15))
+gray = cv2.cvtColor(blured, cv2.COLOR_BGR2GRAY)
+roi = gray[h-250:h,w-250:w]
+    
+_ , thresholded1 = cv2.threshold(roi, 127, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+'''
+transformed = cv2.distanceTransform(thresholded, cv2.DIST_L2, 3)
+_ , trans_thresholded40 = cv2.threshold(transformed, 0.40*transformed.max(),255,0)
+_ , trans_thresholded10 = cv2.threshold(transformed, 0.10*transformed.max(),255,0)
+diff1 = cv2.absdiff(trans_thresholded10,trans_thresholded40)
+#diff1 = cv2.erode(diff1, (5,5), 3)
+'''
+while True:
+    
+    ret,frame = cam.read()
+    blured = cv2.blur(frame.copy(), (5,5))
+    gray = cv2.cvtColor(blured, cv2.COLOR_BGR2GRAY)
+    roi = gray[h-250:h,w-250:w]
+    cv2.rectangle(frame, (w-250,h-250) , (w,h), 255, 5)
+    
+    _ , thresholded2 = cv2.threshold(roi, 127, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    '''
+    transformed = cv2.distanceTransform(thresholded, cv2.DIST_L2, 3)
+    _ , trans_thresholded40 = cv2.threshold(transformed, 0.40*transformed.max(),255,0)
+    _ , trans_thresholded10 = cv2.threshold(transformed, 0.10*transformed.max(),255,0)
+    diff2= cv2.absdiff(trans_thresholded10,trans_thresholded40)
+    #diff2 = cv2.erode(diff2, (5,5), 3)
+    '''
+    '''
+    diff = cv2.absdiff(diff1,diff2)
+    #diff = cv2.blur(diff,(5,5))
+    diff = cv2.erode(diff, (5,5), 1000)
+    #diff = cv2.distanceTransform(diff, cv2.DIST_L2, 5)
+    
+    diff = np.expand_dims(diff, axis = 2)
+    '''
+    diff = cv2.absdiff(thresholded1,thresholded2)
+    #diff = cv2.blur(diff,(5,5))
+    #diff = cv2.erode(diff, (5,5), 1000)
+    #diff = cv2.distanceTransform(diff, cv2.DIST_L2, 5)
+    diff = cv2.distanceTransform(diff, cv2.DIST_L2, 3)
+    _,diff = cv2.threshold(diff, 0.4*diff.max(), 255, cv2.THRESH_BINARY)
+    diff = np.expand_dims(diff, axis = 2)
+
+    frame[h-250:h,w-250:w] = diff
+
+    
+    cv2.imshow('frame',frame)
+    
+    if cv2.waitKey(10) & 0xFF == 27:
+        print(frame.shape)
+        break
+
+cam.release()
+cv2.destroyAllWindows()
