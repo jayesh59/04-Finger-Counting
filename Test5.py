5eaa29ecd767fc17b7c30eeab09faa3ab657c033
jayesh59 <51175318+jayesh59@users.noreply.github.com>
2020-04-17 15:00:58 +0530

    Update Test5.py
    so the problem has been that there is assertion in draw contour function and that might be that the first contour might not be very filled up
    so now will try to find the length od contour and then will try to find what is beng stored as contour image.

--- Test5.py
+++ Test5.py
@@ -9,14 +9,17 @@ accum_weight = 0.5
 def calc_avg(frame, accum_weight):
     global background
 
-    if background == None:
+    if background is None:
         background = frame.copy().astype('float')
     
         return None
+    else:
+        pass
 
     cv2.accumulateWeighted(frame, background, accum_weight)
 
 def segment(frame, thresh_min = 25):
+    global background
 
     diff = cv2.absdiff(frame,background.astype('uint8'))
 
@@ -42,7 +45,7 @@ def display(frame,frame_dict):
     return None    
 
 def diff_contours(dist_trans):
-    
+    dist_trans = cv2.cvtColor(dist_trans, cv2.COLOR_BGR2GRAY)
     _ , Dist_Thresh10 = cv2.threshold(dist_trans, 0.1*dist_trans.max(), 255, cv2.THRESH_BINARY)
     _ , Dist_Thresh40 = cv2.threshold(dist_trans, 0.4*dist_trans.max(), 255, cv2.THRESH_BINARY)
     fingers = cv2.absdiff(Dist_Thresh10, Dist_Thresh40)
@@ -51,12 +54,13 @@ def diff_contours(dist_trans):
 
 cap = cv2.VideoCapture(0)
 
+_, frame = cap.read()
 h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 num_frames = 0
 
-black = np.zeros((250,250))
-
+black = frame.copy()[h-250:h,w-250:w]
+#black = black[:,:,:
 while True:
 
     _, frame = cap.read()
@@ -68,15 +72,20 @@ while True:
 
     else:
 
-        th, cont = segment(frame[h-250:h,w-250:w],127)
+        a = segment(frame[h-250:h,w-250:w])
+        
+        if a is not None:
+            th, cont = a
 
-        cont_img = cv2.drawContours(black, cont, 0, [255,255,255], -1)
-        dist_trans = cv2.distanceTransform(cont_img, cv2.DIST_L2, 3)
-        Dist_Thresh10, Dist_Thresh40, fingers = diff_contours(dist_trans)
+            cont_img = cv2.drawContours(black, cont, 0, [255,255,255], -1)
+            #cont_img = np.expand_dims(cont_img, axis = 2)
+            dist_trans = cv2.distanceTransform(cont_img, cv2.DIST_L2, 3)
+            b = diff_contours(dist_trans)
+            Dist_Thresh10, Dist_Thresh40, fingers = b
 
-        frame_dict = {'Thresholded':th, 'Contour':cont_img, 'Distance Transformation':dist_trans, '10% Distance Threshold':Dist_Thresh10, '40% Distance Threshold':Dist_Thresh40, 'Fingers':fingers}
+            frame_dict = {'Thresholded':th, 'Contour':cont_img, 'Distance Transformation':dist_trans, '10% Distance Threshold':Dist_Thresh10, '40% Distance Threshold':Dist_Thresh40, 'Fingers':fingers}
 
-        display(frame, frame_dict)
+            display(frame, frame_dict)
 
     num_frames += 1
 
